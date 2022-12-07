a9238e08ee1d022e6812a008b9c6c81529f4a222
Johnson <g00389668@atu.ie>
2022-12-07 12:07:36 +0000

    new methods for name added

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -3,6 +3,15 @@ public class Employee {
     private long pps;
 
     public Employee(){
+        setName(name);
+    }
 
+    public void setName(String name) {
+        if(name.length() >= 5 || name.length() <= 22){
+            this.name = name;
+        }
+        else{
+            throw new IllegalArgumentException("Name provided is not valid");
+        }
     }
 }
