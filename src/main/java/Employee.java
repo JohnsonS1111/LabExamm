cbd8b2ee482f20ab7390d272fd792409ca21cf97
Johnson <g00389668@atu.ie>
2022-12-07 12:11:18 +0000

    new methods for pps added

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,8 +1,17 @@
 public class Employee {
     private String name;
-    private long pps;
+    private String pps;
 
     public Employee(){
+        setPPS(pps);
+    }
 
+    public void setPPS(String pps) {
+        if(pps.length() == 6){
+            this.pps = pps;
+        }
+        else{
+            throw new IllegalArgumentException("Employee PPS should be 6 characters long");
+        }
     }
 }
